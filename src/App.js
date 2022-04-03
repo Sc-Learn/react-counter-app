447434e26ab416f98d423270c6418c939fff2b15
zidanesc <zidankren02@gmail.com>
2022-04-03 21:12:21 +0700

    divide component to new file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,92 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
-import './App.css';
+import Navbar from './components/Navbar';
+import Container from './components/Container';
+import SearchInput from './components/SearchInput';
 
-import shoppingIcon from './assets/shopping-icon.svg';
+import Info from './components/Info';
+import Todos from './components/Todos';
+import Empty from './components/Empty';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([
+    { title: 'Susu Ultra', count: 1 },
+    { title: 'Tahu Sumedang', count: 1 },
+    { title: 'Semangka', count: 1 },
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert('No blank list');
+      return false
+    }
+
+    const addedTodos = [{
+      title: value,
+      count: 1
+    }, ...todos]
+
+    setTodos(addedTodos);
+    setValue('');
+  }
+
+  const handleAdditionCount = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].count += 1;
+
+    setTodos(newTodos);
+  }
+
+  const handleSubstractionCount = (index) => {
+    const newTodos = [...todos];
+
+    if (newTodos[index].count > 0) {
+      newTodos[index].count -= 1;
+    } else {
+      newTodos.splice(index, 1);
+    }
+
+    setTodos(newTodos);
+  }
+
+  const getTotalCounts = () => {
+    const totalCounts = todos.reduce((total, todo) => {
+      return total + todo.count;
+    }, 0);
+
+    return totalCounts;
+  }
+
   return (
     <>
-      <nav className="nav">
-        <img className="nav-icon" src={ shoppingIcon } alt="shopping icon" />
-        <h1 className='nav-title'>Shopping List</h1>
-      </nav>
+      <Navbar />
 
-      <section className="container">
-        
-      </section>
+      <Container>
+        <SearchInput
+          onSubmit={handleSubmit}
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+
+        <Info
+          todosLength={todos.length}
+          totalCounts={getTotalCounts()}
+          onDelete={() => setTodos([])}
+        />
+
+        {todos.length > 0 ? (
+          <Todos 
+            todos={todos}
+            onSubstraction={(index) => handleSubstractionCount(index)}
+            onAddition={(index) => handleAdditionCount(index)}
+          />
+        ) : (
+          <Empty />
+        )}
+      </Container>
     </>
   );
 }
